4553869a99f23c57efdade8a6f1d3c0d73282c3e
MrMuhammadjon <asliyevmukhammadjon@gmail.com>
2025-05-19 23:52:16 +0500

    the end for today

--- src/Components/BestSeller.jsx
+++ src/Components/BestSeller.jsx
@@ -4,14 +4,18 @@ import { useAppContext } from '../context/AppContext'
 
 const BestSeller = () => {
 
-    const {products} = useAppContext()
+    const { products } = useAppContext()
 
     return (
         <>
             <div className="w-[90%] flex flex-col gap-3 m-auto mt-10 py-10">
                 <h1 className='text-2xl md:text3xl font-medium text-green-600'>Best Seller</h1>
-                <div className="w-[100%] grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6">
-                    <Products product={products[0]}/>
+                <div className="w-[100%] grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 md:gap-6 lg:grid-cols-5  gap-3">
+                    {
+                        products.filter((product) => product.inStock).slice(0,5).map((product, index)=>(
+                            <Products key={index} product={product}/>
+                        ))
+                    }
                 </div>
             </div>
         </>
